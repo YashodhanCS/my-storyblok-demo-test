532538f9c0c021f89fd7a42061680262d760872c
YashodhanCS <yashodhan.advankar@candyspace.com>
2024-09-25 15:35:43 +0530

    updating env vars

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -3,6 +3,7 @@ import { storyblokEditable } from "@storyblok/react";
 const Feature = ({ blok }) => (
   <div {...storyblokEditable(blok)} className="column feature">
     {blok.name}
+    <h1>Elon musk</h1>
   </div>
 );
 
